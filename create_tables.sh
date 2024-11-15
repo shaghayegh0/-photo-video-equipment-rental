0abe4b6ba35481bba9bb3084f8d429820471aca8
shaghayegh0 <119910382+shaghayegh0@users.noreply.github.com>
2024-11-15 11:08:46 -0500

    Update create_tables.sh
    modified all tables based on lab8

--- create_tables.sh
+++ create_tables.sh
@@ -1,48 +1,58 @@
 #!/bin/sh
 sqlplus64 "d1yeung/06145080@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.cs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
--- Create independent tables
+
+CREATE TABLE Equipment_Type (
+    Equipment_Type_ID INT GENERATED AS IDENTITY PRIMARY KEY,
+    Equipment_Type_Name  VARCHAR(100) NOT NULL UNIQUE
+);
+
 CREATE TABLE Customer (
-    Customer_ID INT PRIMARY KEY,
+    Customer_ID INT GENERATED AS IDENTITY PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
-    Email VARCHAR(100) NOT NULL UNIQUE,
-    Phone VARCHAR(20) NOT NULL UNIQUE,
     Address VARCHAR(255)
 );
 
-CREATE TABLE Equipment_Type (
-    Equipment_Type_ID INT PRIMARY KEY,
-    Equipment_Type_Name VARCHAR(100) NOT NULL UNIQUE
-);
-
 CREATE TABLE Staff (
-    Staff_ID INT PRIMARY KEY,
+    Staff_ID INT GENERATED AS IDENTITY PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
     Position VARCHAR(50) NOT NULL
 );
 
--- Create dependent tables
 CREATE TABLE Equipment (
-    Equipment_ID INT PRIMARY KEY,
+    Equipment_ID INT GENERATED AS IDENTITY PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
     Equipment_Type_ID INT NOT NULL,
-    Rental_Status VARCHAR(50) DEFAULT 'Available' CHECK (Rental_Status IN ('Available', 'Rented', 'Under Maintenance', 'Out of Service')),
-    Rental_Price_PerDay DECIMAL(10, 2) NOT NULL,
+    Status VARCHAR(50) DEFAULT 'Available' CHECK (Rental_Status IN ('Available', 'Under Maintenance', 'Out of Service')),
     Condition VARCHAR(100) NOT NULL,
     Purchase_Date DATE NOT NULL,
-    Equipment_price DECIMAL(10, 2) NOT NULL,
+    Equipment_Price DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (Equipment_Type_ID) REFERENCES Equipment_Type(Equipment_Type_ID)
 );
 
+CREATE TABLE Customer_Phone (
+    Phone_ID INT GENERATED AS IDENTITY PRIMARY KEY,
+    Customer_ID INT,
+    Customer_Phone_Number VARCHAR(20) NOT NULL UNIQUE,
+    CONSTRAINT fk_customer FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
+);
+
+CREATE TABLE Customer_Email (
+    Email_ID INT GENERATED AS IDENTITY PRIMARY KEY, 
+    Customer_ID INT,
+    Customer_Email VARCHAR(100) NOT NULL UNIQUE,
+    CONSTRAINT fk_customer_email FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID) 
+);
+
 CREATE TABLE Rental (
-    Rental_ID INT PRIMARY KEY,
+    Rental_ID INT GENERATED AS IDENTITY PRIMARY KEY,
     Staff_ID INT,
     Customer_ID INT,
     Equipment_ID INT,
     Rental_Date DATE NOT NULL,
     Estimated_Return_Date DATE NOT NULL,
     Estimated_Duration_Days INT,
-    Price DECIMAL(10, 2),
+    Rentail_Price DECIMAL(10, 2) NOT NULL,
     Status VARCHAR(50) DEFAULT 'Ongoing' CHECK (Status IN ('Ongoing', 'Completed', 'Overdue')),
     FOREIGN KEY (Staff_ID) REFERENCES Staff(Staff_ID),
     FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID),
@@ -50,16 +60,19 @@ CREATE TABLE Rental (
 );
 
 CREATE TABLE Rental_History (
-    Rental_History_ID INT PRIMARY KEY,
-    Rental_ID INT,
-    Return_Date DATE NOT NULL,
-    FOREIGN KEY (Rental_ID) REFERENCES Rental(Rental_ID)
+    Rental_History_ID INT GENERATED AS IDENTITY PRIMARY KEY,
+    Staff_ID INT,
+    Customer_ID INT,
+    Equipment_ID INT,
+    Rental_Date DATE NOT NULL,
+    Duration INT,
+    Rental_Return DATE NOT NULL,
+    Price DECIMAL(10, 2)
 );
 
 CREATE TABLE Overdue (
-    Overdue_ID INT PRIMARY KEY,
+    Overdue_ID INT GENERATED AS IDENTITY PRIMARY KEY,
     Rental_ID INT,
-    Customer_ID INT,
     Overdue_Date DATE NOT NULL,
     Amount DECIMAL(10, 2),
     Payment_Method VARCHAR(50),
@@ -68,10 +81,16 @@ CREATE TABLE Overdue (
     FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
 );
 
+CREATE TABLE Rental_Customer (
+    Rental_ID INT PRIMARY KEY,
+    Customer_ID INT NOT NULL,
+    FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
+);
+
+
 CREATE TABLE Payment (
-    Payment_ID INT PRIMARY KEY,
+    Payment_ID INT GENERATED AS IDENTITY PRIMARY KEY,
     Rental_ID INT,
-    Customer_ID INT,
     Payment_Date DATE NOT NULL,
     Amount DECIMAL(10, 2),
     Payment_Method VARCHAR(50),
@@ -81,20 +100,26 @@ CREATE TABLE Payment (
 );
 
 CREATE TABLE Payment_History (
-    Payment_History_ID INT PRIMARY KEY,
-    Payment_ID INT,
-    Rental_ID INT,
-    Customer_ID INT,
+    Payment_History_ID INT GENERATED AS IDENTITY PRIMARY KEY,
+    Payment_ID INT NOT NULL,
     Payment_Date DATE NOT NULL,
     Amount DECIMAL(10, 2),
     Payment_Method VARCHAR(50),
-    FOREIGN KEY (Payment_ID) REFERENCES Payment(Payment_ID),
+    FOREIGN KEY (Payment_ID) REFERENCES Payment(Payment_ID)
+);
+
+CREATE TABLE Payment_Rental_Customer (
+    Payment_ID INT PRIMARY KEY,
+    Rental_ID INT NOT NULL,
+    Customer_ID INT NOT NULL,
     FOREIGN KEY (Rental_ID) REFERENCES Rental(Rental_ID),
     FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
 );
 
+
+
 CREATE TABLE Reservation (
-    Reservation_ID INT PRIMARY KEY,
+    Reservation_ID INT GENERATED AS IDENTITY PRIMARY KEY,
     Customer_ID INT,
     Equipment_ID INT,
     Reservation_Date DATE NOT NULL,
@@ -104,22 +129,8 @@ CREATE TABLE Reservation (
     FOREIGN KEY (Equipment_ID) REFERENCES Equipment(Equipment_ID)
 );
 
-CREATE TABLE Customer_Phone (
-    Phone_ID INT PRIMARY KEY,
-    Customer_ID INT,
-    Customer_Phone_Number VARCHAR(20) NOT NULL UNIQUE,
-    CONSTRAINT fk_customer FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
-);
-
-CREATE TABLE Customer_Email (
-    Email_ID INT PRIMARY KEY, 
-    Customer_ID INT,
-    Customer_Email VARCHAR(100) NOT NULL UNIQUE,
-    CONSTRAINT fk_customer_email FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID) 
-);
-
 CREATE TABLE Maintenance (
-    Maintenance_ID INT PRIMARY KEY,
+    Maintenance_ID INT GENERATED AS IDENTITY PRIMARY KEY,
     Staff_ID INT,
     Equipment_ID INT,
     Maintenance_Start_Date DATE NOT NULL,
